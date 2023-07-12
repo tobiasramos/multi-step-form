05a0c39da9762405bb077a18433a9d4326290d1c
Tobias Ramos <tobiasramossilva@gmail.com>
2023-07-12 18:46:49 -0300

    Adicionando o conteúdo do componente de escolher o plano e já fazendo a rota para ser direcionado para o próximo componente

--- src/components/selectYourPlan/select-your-plan.js
+++ src/components/selectYourPlan/select-your-plan.js
@@ -1,22 +1,48 @@
 import { styled } from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const SelectYourPlan = () => {
+  const history = useHistory();
+
+  const handleNextStep = () => {
+    history.push("/pick-add-ons");
+  };
   return (
-    <div>
+    <PlanContainer>
       <h1>Selecione o seu plano</h1>
-      <PlanContainer>
-        <Plan></Plan>
-        <Plan></Plan>
-        <Plan></Plan>
-      </PlanContainer>
-    </div>
+      <p>Você tem a opção de cobrança anual.</p>
+      <div>
+        <Plan>
+          <Img src="/images/icon-arcade.svg" alt="Icone arcade" />
+          <spn>Arcade</spn>
+          <span>$9/mo</span>
+        </Plan>
+        <Plan>
+          <Img src="/images/icon-advanced.svg" alt="Icone advanced" />
+          <spn>Advanced</spn>
+          <span>$12/mo</span>
+        </Plan>
+        <Plan>
+          <Img src="/images/icon-pro.svg" alt="Icone pro" />
+          <span>Pro</span>
+          <span>$15/mo</span>
+        </Plan>
+      </div>
+      <button onClick={handleNextStep}>Próxima etapa</button>
+    </PlanContainer>
   );
 };
 
-const PlanContainer = styled.div`
-display: flex:
+const PlanContainer = styled.div``;
+
+const Plan = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid hsl(231, 11%, 63%);
 `;
 
-const Plan = styled.div``;
+const Img = styled.img`
+  width: 50px;
+`;
 
 export default SelectYourPlan;
