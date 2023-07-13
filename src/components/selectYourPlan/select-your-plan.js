20db5d2c1c5eb27413181c5e9590ce746e4ad0fa
Tobias Ramos <tobiasramossilva@gmail.com>
2023-07-13 15:47:30 -0300

    Fazendo estilizações no componente selecYourPlan

--- src/components/selectYourPlan/select-your-plan.js
+++ src/components/selectYourPlan/select-your-plan.js
@@ -11,38 +11,61 @@ const SelectYourPlan = () => {
     <PlanContainer>
       <h1>Selecione o seu plano</h1>
       <p>Você tem a opção de cobrança anual.</p>
-      <div>
+      <Plans>
         <Plan>
           <Img src="/images/icon-arcade.svg" alt="Icone arcade" />
-          <spn>Arcade</spn>
-          <span>$9/mo</span>
+          <TypePlan>Arcade</TypePlan>
+          <Price>R$9,00/mês</Price>
         </Plan>
         <Plan>
           <Img src="/images/icon-advanced.svg" alt="Icone advanced" />
-          <spn>Advanced</spn>
-          <span>$12/mo</span>
+          <TypePlan>Advanced</TypePlan>
+          <Price>R$12,00/mês</Price>
         </Plan>
         <Plan>
           <Img src="/images/icon-pro.svg" alt="Icone pro" />
-          <span>Pro</span>
-          <span>$15/mo</span>
+          <TypePlan>Pro</TypePlan>
+          <Price>R$15,00/mês</Price>
         </Plan>
-      </div>
+      </Plans>
       <button onClick={handleNextStep}>Próxima etapa</button>
     </PlanContainer>
   );
 };
 
-const PlanContainer = styled.div``;
+const PlanContainer = styled.div`
+  border: 1px red solid;
+  width: 25rem;
+`;
+
+const Plans = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const Plan = styled.div`
   display: flex;
   flex-direction: column;
+  margin: auto 10px;
+  padding: 5px;
+  width: 30%;
+  cursor: pointer;
   border: 1px solid hsl(231, 11%, 63%);
+  border-radius: 10px;
 `;
 
 const Img = styled.img`
   width: 50px;
+  margin-bottom: 30px;
+`;
+
+const TypePlan = styled.span`
+  font-weight: 500;
+`;
+
+const Price = styled.span`
+  margin-top: 5px;
+  font-size: 14px;
 `;
 
 export default SelectYourPlan;
