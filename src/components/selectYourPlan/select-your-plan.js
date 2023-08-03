249e554093066fdfd4ea4afa5e6df0c872f72816
Tobias Ramos <tobiasramossilva@gmail.com>
2023-08-03 16:58:39 -0300

    Criando um  switch para o usuário escolher o plano mesal ou anual

--- src/components/selectYourPlan/select-your-plan.js
+++ src/components/selectYourPlan/select-your-plan.js
@@ -7,10 +7,10 @@ const SelectYourPlan = () => {
   const [selectedPlan, setSelectedPlan] = useState(null);
 
   const handleNextStep = () => {
-    if(selectedPlan){
+    if (selectedPlan) {
       history.push("/pick-add-ons");
-    }else {
-      alert("Selecione um plano")
+    } else {
+      alert("Selecione um plano");
     }
   };
 
@@ -18,27 +18,84 @@ const SelectYourPlan = () => {
     setSelectedPlan(plan);
   };
 
+  const [checked, setChecked] = useState(false);
+
+  const handleSwitchChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   return (
     <PlanContainer>
       <h1>Selecione o seu plano</h1>
       <p>Você tem a opção de cobrança anual.</p>
       <Plans>
-      <Plan onClick={() => handlePlanClick("arcade")} selected={selectedPlan === "arcade"}>
+        <Plan
+          onClick={() => handlePlanClick("arcade")}
+          selected={selectedPlan === "arcade"}
+        >
           <Img src="/images/icon-arcade.svg" alt="Icone arcade" />
-          <TypePlan>Arcade</TypePlan>
-          <Price>R$9,00/mês</Price>
+          {checked ? (
+            <Description>
+              <TypePlan>Arcade</TypePlan>
+              <Price>R$90,00/ano</Price>
+              <Free>2 meses grátis</Free>
+            </Description>
+          ) : (
+            <Description>
+              <TypePlan>Arcade</TypePlan>
+              <Price>R$9,00/mês</Price>
+            </Description>
+          )}
         </Plan>
-        <Plan onClick={() => handlePlanClick("advanced")} selected={selectedPlan === "advanced"}>
+        <Plan
+          onClick={() => handlePlanClick("advanced")}
+          selected={selectedPlan === "advanced"}
+        >
           <Img src="/images/icon-advanced.svg" alt="Icone advanced" />
-          <TypePlan>Advanced</TypePlan>
-          <Price>R$12,00/mês</Price>
+          {checked ? (
+            <Description>
+              <TypePlan>Advanced</TypePlan>
+              <Price>R$120,00/ano</Price>
+              <Free>2 meses grátis</Free>
+            </Description>
+          ) : (
+            <Description>
+              <TypePlan>Advanced</TypePlan>
+              <Price>R$12,00/mês</Price>
+            </Description>
+          )}
         </Plan>
-        <Plan onClick={() => handlePlanClick("pro")} selected={selectedPlan === "pro"}>
+        <Plan
+          onClick={() => handlePlanClick("pro")}
+          selected={selectedPlan === "pro"}
+        >
           <Img src="/images/icon-pro.svg" alt="Icone pro" />
-          <TypePlan>Pro</TypePlan>
-          <Price>R$15,00/mês</Price>
+          {checked ? (
+            <Description>
+              <TypePlan>Pro</TypePlan>
+              <Price>R$150,00/ano</Price>
+              <Free>2 meses grátis</Free>
+            </Description>
+          ) : (
+            <Description>
+              <TypePlan>Pro</TypePlan>
+              <Price>R$15,00/mês</Price>
+            </Description>
+          )}
         </Plan>
       </Plans>
+      <SwitchContainer>
+      <span className={checked ? 'inactive' : 'active'}>Mensal</span>
+        <Switch>
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={handleSwitchChange}
+          />
+          <span></span>
+        </Switch>
+        <span className={checked ? 'active' : 'inactive'}>Anual</span>
+      </SwitchContainer>
       <button onClick={handleNextStep}>Próxima etapa</button>
     </PlanContainer>
   );
@@ -64,7 +121,6 @@ const Plan = styled.div`
   border: 1px solid hsl(231, 11%, 63%);
   border-radius: 10px;
 
-
   ${(props) =>
     props.selected &&
     `
@@ -77,13 +133,87 @@ const Img = styled.img`
   margin-bottom: 30px;
 `;
 
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const TypePlan = styled.span`
   font-weight: 500;
+  font-size: 16px;
 `;
 
 const Price = styled.span`
   margin-top: 5px;
+  font-size: 16px;
+`;
+
+const Free = styled.span`
   font-size: 14px;
+  color: hsl(243, 100%, 62%);
+`;
+
+const SwitchContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  span.active {
+    color: hsl(213, 96%, 18%);
+  }
+
+  span.inactive {
+    color: hsl(231, 11%, 63%);
+  }
+`;
+
+const Switch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+
+  span {
+    position: absolute;
+    cursor: pointer;
+    border-radius: 34px;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: hsl(213, 96%, 18%);
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  span:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    border-radius: 50%;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  input:checked + span {
+    background-color: hsl(213, 96%, 18%);
+  }
+
+  input:focus + span {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + span:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 `;
 
 export default SelectYourPlan;
